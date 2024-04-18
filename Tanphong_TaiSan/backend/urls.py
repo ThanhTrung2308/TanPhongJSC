35e5bf41280b11b91be2408e9f65ba71b67e821b
ThanhTrung2308 <trungk53cma@gmail.com>
2024-04-19 00:28:04 +0700

    update views 2

--- Tanphong_TaiSan/backend/urls.py
+++ Tanphong_TaiSan/backend/urls.py
@@ -5,5 +5,9 @@ from . import views
 urlpatterns = [
     path('hopdong/',views.HopDongAPIView.as_view()),
     path('dichvu/',views.DichVuAPIView.as_view()),
+    path('hopdongdichvu/',views.HopDongDichVuAPIView.as_view()),
+    path('loaidichvu/',views.LoaiDichVuAPIView.as_view()),
+    path('taisan/',views.TaiSanAPIView.as_view()),
+    path('loaitaisan/',views.LoaiTaiSanAPIView.as_view()),
     path('',views.index)
 ]

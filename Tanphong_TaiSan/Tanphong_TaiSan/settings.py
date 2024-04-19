00d70079dddeb8414e14f617655d3b468d43a54a
unknown <thanhd436@gmail.com>
2024-04-19 23:08:50 +0700

    update cors

--- Tanphong_TaiSan/Tanphong_TaiSan/settings.py
+++ Tanphong_TaiSan/Tanphong_TaiSan/settings.py
@@ -133,4 +133,9 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 ALLOWED_HOSTS = ['tanphongjsc.onrender.com', 'localhost', '127.0.0.1']
 CSRF_TRUSTED_ORIGINS = ['https://tanphongjsc.onrender.com']
+
 CORS_ALLOW_ALL_ORIGINS = True
+CORS_ALLOW_CREDENTIALS = True
+CORS_ORIGIN_WHITELIST = (
+  'http://localhost:8000',
+)

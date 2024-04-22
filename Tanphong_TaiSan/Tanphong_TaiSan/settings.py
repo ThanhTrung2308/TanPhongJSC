73fb5a3412b0801bfcc35587c79873940502d102
ThanhTrung2308 <trungk53cma@gmail.com>
2024-04-22 22:18:36 +0700

    update setting append slash

--- Tanphong_TaiSan/Tanphong_TaiSan/settings.py
+++ Tanphong_TaiSan/Tanphong_TaiSan/settings.py
@@ -126,7 +126,7 @@ USE_TZ = True
 
 STATIC_URL = 'static/'
 
-APPEND_SLASH = True
+APPEND_SLASH = False
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
